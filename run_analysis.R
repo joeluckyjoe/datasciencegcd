bb961b836e18b6b8a696471d43adb63c331e6fb9
Giuseppe Albanese <joeluckyjoe@hotmail.com>
2014-06-19 22:34:08 +0200

    used join() for part #3, modified the column names

--- run_analysis.R
+++ run_analysis.R
@@ -17,7 +17,7 @@
 
 # create a directory where we will download the zip file
 
-setwd("C:/Users/P06226/")
+#setwd("C:/Users/P06226/")
 
 if (!file.exists("gcddata")) {
         dir.create("gcddata")
@@ -26,50 +26,48 @@ if (!file.exists("gcddata")) {
 # download the zip file ...
 
 zipUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+
 if (!file.exists("./gcddata/Dataset.zip")) {
         download.file(zipUrl, destfile = "./gcddata/Dataset.zip",method = "internal", mode="wb")
 }
+
 list.files("./gcddata")
 
-# get the acceleration signal files, the body acceleration signal files
-#  and the angular velocity vector file
-#total_acc_x_trainunz <- unz("./gcddata/Dataset.zip",
-#                            "UCI HAR Dataset/train/Inertial Signals\total_acc_x_train.txt")
-#total_acc_x_train <- read.table(total_acc_x_trainunz, header=FALSE)
-# ....
 
 # get training set
 
 trainingunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/train/X_train.txt")
+
 trainingset <- read.table(trainingunz, header=FALSE) 
-#close(trainingunz)
 
 # get training labels
 
 traininglabelsunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/train/y_train.txt")
+
 traininglabels <- read.table(traininglabelsunz, header=FALSE) 
-#close(traininglabelsunz)
 
 # get training subjects
 
 trainingsubjectsunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/train/subject_train.txt")
+
 trainingsubjects <- read.table(trainingsubjectsunz, header=FALSE)
 
 # get test set
 
 testunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/test/X_test.txt")
+
 testset <- read.table(testunz, header=FALSE) 
-#close(testunz)
 
 # get test labels
 
 testlabelsunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/test/y_test.txt")
+
 testlabels <- read.table(testlabelsunz, header=FALSE) 
-#close(testlabelsunz)
 
 # get test subjects
 
 testsubjectsunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/test/subject_test.txt")
+
 testsubjects <- read.table(testsubjectsunz, header=FALSE)
 
 # merge training and test sets in one set
@@ -81,7 +79,9 @@ measurements <- rbind(trainingset, testset)
 # the second variable (V2) contains the name of the feature
 
 featuresunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/features.txt")
+
 features <- read.table(featuresunz, header=FALSE) 
+
 featurenames <- features[,2]
 
 # merge training and test labels in one set
@@ -92,23 +92,24 @@ labels <- rbind(traininglabels, testlabels)
 
 subjects <- rbind(trainingsubjects, testsubjects)
 
-        
+
 # get the activity labels
 
 activitiesunz <- unz("./gcddata/Dataset.zip","UCI HAR Dataset/activity_labels.txt")
+
 activities <- read.table(activitiesunz, header=FALSE) 
+
 activitynames <- activities[,2]
 
 
 # 2 Extract only the measurements on the mean and standard deviation for each measurement.
 
 # find the position for the feature names that contain -mean() or -std() using grepl()
-#      ... note that we could have chosen to find -mean or -std feature names
 # put the result in submeasurementspos that will be an index for subsetting
 # put the corresponding names in submeasurementsnames
 
 submeasurementspos <- grepl(".*-(mean|std)\\(\\).*", featurenames)
-#submeasurementspos <- grepl(".*-(std|mean)\\(\\).*", featurenames)
+
 submeasurementsnames <- featurenames[submeasurementspos]
 
 # Extract only the measurements on the mean and standard deviation for each measurement based
@@ -116,52 +117,43 @@ submeasurementsnames <- featurenames[submeasurementspos]
 
 measurements <- measurements[,submeasurementspos]
 
-
 # 3 Use descriptive activity names to name the activities in the data set
 
-labels <- as.data.frame(lapply(labels, f <- function(x) {x <- activitynames[x] }))
-names(labels) <- c("activity")
+library(plyr)
+
+labels <- join(labels,activities)
+
 
 # 4 Appropriately label the data set with descriptive variable names. 
 
-names(measurements) <- submeasurementsnames
-names(labels) <- c("activity")
+names(measurements) <- tolower(gsub("\\(\\)","",gsub("-","",submeasurementsnames)))
+
+names(labels) <- c("activity","activityname")
+
 names(subjects) <- c("subject")
 
 # here's the first dataset ...
 
-measurements <- cbind(subjects,labels, measurements)
+measurements <- cbind(subjects,labels$activityname, measurements)
+
+names(measurements)[2] <- "activity"
 
 
 # 5 Create a second, independent tidy data set with the average of each variable for each activity 
 #   and each subject. 
 
-# melt the measurements
+library(plyr)
 
-library(reshape2)
-mmeasurements <- melt(measurements, id=c("subject", "activity"))
+tdm <- ddply(measurements,.(activity,subject),numcolwise(mean),.drop=TRUE)
 
-# cast the molten data
+names(tdm)[3:length(names(tdm))] <- sub("(.*)","avg\\1",names(tdm)[3:length(names(tdm))])
 
-#cmeasurements <- acast(mmeasurements, subject ~ activity ~ variable, mean)
 
-cmeasurements <- acast(mmeasurements, subject + activity ~ variable , mean)
+# write tdm to a file using write.table
 
-#get the subects from the row.names  of the cast data
+write.table(tdm, file= "./gcddata/tidymeasurements.txt", row.names = FALSE)
 
-csubjects <- sub("([0-9]*)\\_.*","\\1",row.names(cmeasurements))
+list.files("./gcddata")
 
-#get the activities from the row.names  of the cast data
-
-cactivities <- sub("([0-9]*)\\_(.*)","\\2",row.names(cmeasurements))
-
-# get the column names of the cast data and prefix them with 'avg-'
-
-ccolumnnames <- attr(cmeasurements,"dimnames")[[2]]
-ccolumnnames <- sub("(.*)","avg\\-\\1",ccolumnnames)
-
-# here's the tidy data set ...
-
-tidymeasurements <- as.data.frame(cbind(csubjects,cactivities,cmeasurements))
-row.names(tidymeasurements) <- NULL
-names(tidymeasurements) <- c("subject","activity",ccolumnnames)
+# read the file back using read.table do not forget to user "header = TRUE"
+#tdm2 <- read.table("./gcddata/tidymeasurements.txt", header = TRUE)
